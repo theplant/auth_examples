9196a217c335d0d7df89af141989b63bf0acf41a
Jinzhu <wosmvp@gmail.com>
2017-08-24 21:45:54 +0800

    Add Authority

--- chi_with_authority/main.go
+++ chi_with_authority/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/qor/auth"
+	"github.com/qor/auth/authority"
 	"github.com/qor/auth_themes/clean"
 	"github.com/qor/middlewares"
 )
@@ -15,18 +17,27 @@ import (
 var db, _ = gorm.Open("sqlite3", "test.db")
 
 func main() {
-	Auth := clean.New(&auth.Config{DB: db})
+	var (
+		Auth      = clean.New(&auth.Config{DB: db})
+		Authority = authority.New(&authority.Config{
+			Auth: Auth,
+			RedirectPathAfterAccessDenied: "/auth/login",
+		})
+	)
+
+	Authority.Register("last_actived_in_half_hour", authority.Rule{TimeoutSinceLastActive: time.Minute * 30})
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
 
-	r.Get("/account", func(w http.ResponseWriter, r *http.Request) {
+	r.With(Authority.Authorize()).Get("/account", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("account page"))
 	})
 
-	r.Get("/account/add_creditcard", func(w http.ResponseWriter, r *http.Request) {
+	r.With(Authority.Authorize("last_actived_in_half_hour")).Get("/account/add_creditcard", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("add credit card"))
 	})
 
