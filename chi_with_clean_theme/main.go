f2fa209e561ef9c10c41043bc8a75781239a3044
Jinzhu <wosmvp@gmail.com>
2017-09-20 11:48:21 +0800

    Add more providers to examples

--- chi_with_clean_theme/main.go
+++ chi_with_clean_theme/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/qor/auth"
+	"github.com/qor/auth/providers/facebook"
+	"github.com/qor/auth/providers/github"
+	"github.com/qor/auth/providers/google"
+	"github.com/qor/auth/providers/twitter"
 	"github.com/qor/auth_themes/clean"
 	"github.com/qor/middlewares"
 )
@@ -16,6 +20,27 @@ var db, _ = gorm.Open("sqlite3", "test.db")
 
 func main() {
 	Auth := clean.New(&auth.Config{DB: db})
+
+	Auth.RegisterProvider(github.New(&github.Config{
+		ClientID:     "client id",
+		ClientSecret: "client secret",
+	}))
+
+	Auth.RegisterProvider(google.New(&google.Config{
+		ClientID:     "client id",
+		ClientSecret: "client secret",
+	}))
+
+	Auth.RegisterProvider(facebook.New(&facebook.Config{
+		ClientID:     "client id",
+		ClientSecret: "client secret",
+	}))
+
+	Auth.RegisterProvider(twitter.New(&twitter.Config{
+		ClientID:     "client id",
+		ClientSecret: "client secret",
+	}))
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
